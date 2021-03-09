8e911cd8dc74aea1db950443d7c0cc5b68c425ea
Lucas Silva Viana <80357765+lucasviana13@users.noreply.github.com>
2021-03-09 18:36:38 -0300

    Add files via upload

---
+++ menu.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<locale.h>
+using namespace std;
+
+int main ()
+{
+    setlocale (LC_ALL, "");
+
+    int op;
+
+    cout<< "----------------------------------------------------"<<endl;
+    cout<< "1 -- EXIBIR ESTOQUE"<<endl;
+    cout<< "2 -- EXIBIR PRODUTOS"<<endl;
+    cout<< "3 -- EXIBIR VENDAS"<<endl;
+    cout<< "4 -- EXIBIR UNIDADES"<<endl;
+    cout<< "5 -- EXIBIR CUSTO ATUAL NO ESTOQUE"<<endl;
+    cout<< "6 -- EXIBIR CUSTO DO ESTOQUE DESEJÁVEL"<<endl;
+    cout<< "7 -- EXIBIR LUCRO TOTAL DAS VENDAS"<<endl;
+    cout<< "8 -- EXIBIR RANKING VENDEDOR"<<endl;
+    cout<< "9 -- EXIBIR PRODUTOS MAIS VENDIDOS"<<endl;
+
+    cout<< "----------------------------------------------------"<<endl;
+    cin>>op;
+
+    switch (op)
+    {
+
+    case 1:
+
+        break;
+    case 2:
+
+        break;
+    case 3:
+
+        break;
+    case 4:
+
+        break;
+    case 5:
+
+        break;
+    case 6:
+
+        break;
+    case 7:
+
+        break;
+    case 8:
+
+        break;
+
+    case 9:
+
+        break;
+    }
+    return 0;
+}
